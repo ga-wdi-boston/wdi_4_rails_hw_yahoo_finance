2bca2400ca146866176bbd7ffa9ceea77c1c7e87
Jordan Morano <jordan.morano@gmail.com>
2014-01-27 18:37:24 -0500

    stocks index - made action in controller and formatted view template

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 
-gem 'yahoofinance'
+gem 'yahoo-finance', github: 'herval/yahoo-finance'
 
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,5 +1,20 @@
+require 'pry'
+
 class StocksController < ApplicationController
   def index
+    top_ten = [
+      'GOOG',
+      'MSFT',
+      'FB',
+      'AMZN',
+      'PCLN',
+      'HPQ',
+      'INTC',
+      'QCOM',
+      'MU',
+      'YHOO'
+    ]
+    @data = YahooFinance.quotes(top_ten, [:name, :last_trade_price])
   end
 
   def show
