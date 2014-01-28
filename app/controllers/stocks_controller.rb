23922e3d5467c43a3ea7a722921d0e68c85f4a00
Becky Bell <rlbell@alum.bu.edu>
2014-01-27 22:33:12 -0500

    added show action to stocks controller

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,8 @@
 class StocksController < ApplicationController
 
 	def index
-		@stocks = YahooFinance.quotes('GOOG', 'MSFT','FB', 'AMZN', 'PCLN', 'HPQ', 'INTC', 'QCOM', 'MU', 'YHOO')
+		stock_symbols = ['GOOG', 'MSFT', 'FB', 'AMZN', 'PCLN', 'HPQ', 'INTC', 'QCOM', 'MU', 'YHOO']
+		@stocks = YahooFinance.quotes(stock_symbols, [:symbol, :open, :high, :low,
+			:last_trade_price ])
 	end
 end
