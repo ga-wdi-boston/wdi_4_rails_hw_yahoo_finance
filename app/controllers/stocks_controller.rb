729713578f13766e3a0886e555882508e5b2c5f7
Jordan Morano <jordan.morano@gmail.com>
2014-01-27 19:00:46 -0500

    show action with detail view of stock

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -14,9 +14,24 @@ class StocksController < ApplicationController
       'MU',
       'YHOO'
     ]
-    @data = YahooFinance.quotes(top_ten, [:name, :last_trade_price])
+    @data = YahooFinance.quotes(top_ten, [:name, :last_trade_price, :symbol])
   end
 
   def show
+    ticker = params[:ticker]
+    @details = [
+      :name,
+      :last_trade_price,
+      :bid,
+      :ask,
+      :volume,
+      :stock_exchange,
+      :dividend_yield,
+      :open,
+      :previous_close,
+      :ebitda,
+      :market_capitalization
+    ]
+    @stock = YahooFinance.quotes([ticker], @details).first
   end
 end
