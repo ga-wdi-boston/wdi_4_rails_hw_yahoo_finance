ea3191b66c8324ca307b5f7c1abe2eb00a708d7b
Becky Bell <rlbell@alum.bu.edu>
2014-01-27 23:54:53 -0500

    created show view for stocks controller

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,6 +3,11 @@ class StocksController < ApplicationController
 	def index
 		stock_symbols = ['GOOG', 'MSFT', 'FB', 'AMZN', 'PCLN', 'HPQ', 'INTC', 'QCOM', 'MU', 'YHOO']
 		@stocks = YahooFinance.quotes(stock_symbols, [:symbol, :open, :high, :low,
-			:last_trade_price ])
+			:last_trade_price])
 	end
+
+	def show
+		@show_ticker = YahooFinance.quotes([params[:ticker]], [:symbol, :last_trade_price, :high]).first
+	end
+
 end
