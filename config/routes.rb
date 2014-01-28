224cf72e42e252e8b6f6b04a08c64cc81f926301
Laure Fischer <laurefischer27@gmail.com>
2014-01-28 09:05:06 -0500

    updated links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Wdi4HwRailsYahooFinance::Application.routes.draw do
   get "/stocks/" => "stocks#index"
-  get "/stocks/:ticker" => "stocks#show"
+  get "/stocks/:ticker" => "stocks#show", as: 'stocks_show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
