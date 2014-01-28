09fd3c0d0fc889c1c3f69514bd80f2dcbe157180
Jordan Morano <jordan.morano@gmail.com>
2014-01-27 19:08:27 -0500

    est root directory pointing to stocks#index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Wdi4HwRailsYahooFinance::Application.routes.draw do
   get "stocks/" => 'stocks#index', as: 'stocks_index'
   get "stocks/:ticker" => 'stocks#show', as: 'stocks_show'
-
+  root to: 'stocks#index'
 end
