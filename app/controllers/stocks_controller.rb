dc74097839f76057caed8c669dffde6a9e8b946d
Becky Bell <rlbell@alum.bu.edu>
2014-01-28 08:57:52 -0500

    added link to stock details page in index

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,12 +2,12 @@ class StocksController < ApplicationController
 
 	def index
 		stock_symbols = ['GOOG', 'MSFT', 'FB', 'AMZN', 'PCLN', 'HPQ', 'INTC', 'QCOM', 'MU', 'YHOO']
-		@stocks = YahooFinance.quotes(stock_symbols, [:symbol, :open, :high, :low,
+		@stocks = YahooFinance.quotes(stock_symbols, [:symbol,
 			:last_trade_price])
 	end
 
 	def show
-		@show_ticker = YahooFinance.quotes([params[:ticker]], [:symbol, :last_trade_price, :high]).first
+		@show_ticker = YahooFinance.quotes([params[:ticker]], [:symbol,:open, :high, :low, :last_trade_price, :high]).first
 	end
 
 end
