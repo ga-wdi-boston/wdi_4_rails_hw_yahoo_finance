4e75dd8b05bfc5a4ff78ffd8f46adcf43e6c3365
K Mak <karen.mak@stx.oxon.org>
2014-01-27 19:10:57 -0500

    site done except for links

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,9 +1,15 @@
 class StocksController < ApplicationController
   def index
     @stocks = ["GOOG", "MSFT", "FB", "AMZN", "PCLN", "HPQ", "INTC", "QCOM", "MU", "YHOO"]
-    @data = YahooFinance.quotes(@stocks, [:ask, :last_trade_price]).to_a
+    @data = YahooFinance.quotes(@stocks, [:ask, :last_trade_price])
   end
 
   def show
+    @ticker = params[:ticker]
+    @details = YahooFinance.quotes([@ticker], [:ask, :average_daily_volume, :ask_size, :bid, :ask_real_time, :bid_real_time, :book_value, :bid_size, :chance_and_percent_change, :change, :comission, :change_real_time,:after_hours_change_real_time, :dividend_per_share, :last_trade_date, :earnings_per_share, :error_indicator, :eps_estimate_current_year, :eps_estimate_next_year, :eps_estimate_next_quarter, :holdings_gain_percent, :annualized_gain, :holdings_gain, :low, :high, :low_52_weeks, :high_52_weeks])
+
   end
 end
+
+
+
